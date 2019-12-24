96af1511d512f7e05efb6bacd589160701ac1e94
Petuuuhhh <peterswanton191@bellsouth.net>
2019-12-23 19:30:08 -0500

    o

--- data/mods/rby890/scripts.js
+++ data/mods/rby890/scripts.js
@@ -9,7 +9,7 @@
 /**@type {ModdedBattleScriptsData} */
 let BattleScripts = {
 	inherit: 'gen2',
-	gen: 1,
+	gen: 'rby890',
 	debug(activity) {
 		if (this.format.debug) {
 			this.add('debug', activity);
